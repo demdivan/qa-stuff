8e660ab57d4499becd1356a454468903b271fd71
unknown <8017817vano@gmail.com>
2022-06-21 22:20:36 +0300

    added the new homework 2

---
+++ JS.HW_2.js
@@ -0,0 +1,41 @@
+// 1. Написать скриптик, который сосчитает и выведет результат от возведения 2 в степень 10, начиная со степени 1
+// let chislo = 2
+// for (let stepen=1;stepen<=10;stepen++){
+//    let itog= chislo ** stepen
+//     console.log(itog)
+// }
+
+// 1*. Преобразовать 1 задачу в функцию, принимающую на вход степень, в которую будет возводиться число 2
+//  let myFn = function(chislo,stepen){ 
+//       let result = chislo ** stepen 
+//        console.log(result)
+//       }
+// myFn(2,11)
+
+// 2. Написать скрипт, который выведет 5 строк в консоль таким образом, чтобы в первой строчке выводилось :), во второй :):) и так далее
+// Пример в консоли:
+// :)
+// :):)
+// :):):)
+// :):):):)
+// :):):):):)
+
+// let smile = ':)'
+// for (nextStrochka=1;nextStrochka<=5;nextStrochka++){
+// console.log(smile.repeat(nextStrochka))}
+
+// 2*. Преобразовать 2 задачу в функцию, принимающую на вход строку, которая и будет выводиться в консоль (как в условии смайлик), а также количество строк для вывода 
+// e.g. function printSmile(stroka, numberOfRows)
+// function printSmile(stroka, numberOfRows){
+// for (i=1;i<=numberOfRows;i++){
+//   console.log(stroka.repeat(i))}
+// }
+//   printSmile(':)',5)
+
+
+// 3**.  Написать функцию, которая принимает на вход слово. Задача функции посчитать и вывести в консоль, сколько в слове гласных, и сколько согласных букв.
+// e.g. function getWordStructure(word)
+// В консоли: 
+// Слово (word) состоит из  (число) гласных и (число) согласных букв
+
+// Проверки: 'case', 'Case', 'Check-list'
